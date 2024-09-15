d7fdfad54f77b11726cb123bc6716ffe0457fdfe
Vinh Nguyen Quang <contact.nguyenquangvinh@gmail.com>
2024-09-15 18:15:51 +0700

    first code

---
+++ ASP.NET_MVC/BaiTap3_64132989/BaiTap3_64132989/Controllers/NhanVien_64132989Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap3_64132989.Controllers
+{
+    public class NhanVien_64132989Controller : Controller
+    {
+        // GET: NhanVien_64132989
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.NET_MVC/BaiTap3_64132989/BaiTap3_64132989/Models/EmployeeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaiTap3_64132989.Models
+{
+    public class EmployeeModel
+    {
+        public string id { get; set; }
+        public string fullName { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string department { get; set; }
+    }
+}
