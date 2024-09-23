4c73b696293323214a3537987ec812c24ac13164
Vinh Nguyen Quang <contact.nguyenquangvinh@gmail.com>
2024-09-23 22:05:18 +0700

    Update

---
+++ ASP.NET_MVC/BaiTap6_64132989_IsAuth/BaiTap6_64132989_IsAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BaiTap6_64132989_IsAuth.Startup))]
+namespace BaiTap6_64132989_IsAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
